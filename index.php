7236946ab55a19b2edebf7afc281e9e4c8ab572f
soporte02contaportable <soporte02contaportable@gmail.com>
2026-02-10 10:15:01 -0600

    Corrige comillas en el título de la página principal de WhaFactura

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="node_modules\bootstrap-icons\font\bootstrap-icons.css">
     <link rel="stylesheet" href="css/style.css">
-    <title> "WhaFactura | Crea facturas desde la comodidad de tu WhatsApp"</title>
+    <title> WhaFactura | Crea facturas desde la comodidad de tu WhatsApp</title>
 
     <!-- Google tag (gtag.js) yec: Actualizado 14 dic 2023 -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7Q8Z1DBFJ"></script>
